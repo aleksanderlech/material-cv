e414629fb41e3a39797298aadb3ad15e773e51be
Alfred <alfred@aleksander-lech.com>
2018-07-02 21:45:48 +0400

    Removed unused modules, refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+declare var $: any;
+
 import { Component, Output, EventEmitter } from '@angular/core';
 import { DataService } from './services/data.service';
 import { OnInit } from '@angular/core';
@@ -12,10 +14,48 @@ export class AppComponent implements OnInit {
 
   resume: Resume;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
-      this.dataService.getData().subscribe(data => this.resume = data);
-   }
+    this.enableScrollSpy();
+    this.dataService.getData().subscribe(data => this.resume = data);
+  }
+
+  private enableScrollSpy() {
+
+    /* ======= Scrollspy ======= */
+    $('body').scrollspy({ target: '#page-nav-wrapper', offset: 100 });
+
+    /* ======= ScrollTo ======= */
+    $('.scrollto').on('click', function (e) {
+
+      //store hash
+      var target = this.hash;
+
+      e.preventDefault();
+
+      $('body').scrollTo(target, 800, { offset: -60, 'axis': 'y' });
+
+    });
+
+    /* ======= Fixed page nav when scrolled ======= */
+    $(window).on('scroll resize load', function () {
+
+      $('#page-nav-wrapper').removeClass('fixed');
+
+      var scrollTop = $(this).scrollTop();
+      var topDistance = $('#page-nav-wrapper').offset().top;
+
+      if ((topDistance) > scrollTop) {
+        $('#page-nav-wrapper').removeClass('fixed');
+        $('body').removeClass('sticky-page-nav');
+      }
+      else {
+        $('#page-nav-wrapper').addClass('fixed');
+        $('body').addClass('sticky-page-nav');
+      }
+
+    });
+  }
 
 }
