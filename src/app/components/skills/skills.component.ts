91cb135aefb82b27ea32da72eb2f563469348b27
Aleksander Lech <aleksander@teletronics.ae>
2018-07-02 19:50:52 +0400

    Fixed pie chart initialization

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+declare var $: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cv-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
-
+export class SkillsComponent implements OnInit {
+  ngOnInit() {
+    $('.chart').easyPieChart({
+      barColor: '#00BCD4',//Pie chart colour
+      trackColor: '#e8e8e8',
+      scaleColor: false,
+      lineWidth: 5,
+      animate: 2000,
+      onStep: function (from, to, percent) {
+        $(this.el).find('span').text(Math.round(percent));
+      }
+    });
+  }
 }
