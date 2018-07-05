f8ff9bcc3fe9ead34a05cf520d76c7621b935eca
Alfred <alfred@aleksander-lech.com>
2018-07-05 09:25:19 +0400

    Extracted profile data into model

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from "../../services/resume.model";
 
 @Component({
   selector: 'cv-header',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  
+  @Input() profile: Profile;
 }
