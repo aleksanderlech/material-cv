09db0b9ef0529d52fb468658119e278f45e30efd
Aleksander Lech <me@aleksander-lech.com>
2026-02-10 21:18:52 +0400

    fix: make ng serve render (remove proxy, fetch cv.json, ensure change detection)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 declare var $: any;
 
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { DataService } from './services/data.service';
 import { OnInit } from '@angular/core';
 import { Resume } from "./services/resume.model";
@@ -15,13 +15,20 @@ export class AppComponent implements OnInit {
 
   resume: Resume;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
     // Load data first; also avoid breaking the app if jQuery plugins aren't available yet.
-    this.dataService.getData().subscribe(data => {
-      this.resume = data;
-      this.enableScrollSpy();
+    this.dataService.getData().subscribe({
+      next: (data) => {
+        this.resume = data;
+        // In zoneless/optimized modes, async updates may not trigger change detection.
+        this.cdr.detectChanges();
+        this.enableScrollSpy();
+      },
+      error: (err) => {
+        console.error('Failed to load resume data', err);
+      }
     });
   }
 
